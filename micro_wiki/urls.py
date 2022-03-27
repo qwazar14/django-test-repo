35756da371ce0b145086893b507db14389cdd30c
qwazar14 <zenez89@gmail.com>
2022-03-27 18:52:18 +0300

    Removed mini-wiki

--- micro_wiki/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='wiki-home'),
-    path('  about', views.index, name='wiki-about'),
-    # path('create', views.create, name='create'),
-]
--- micro_wiki/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'micro_wiki/index.html', title='Wiki')
