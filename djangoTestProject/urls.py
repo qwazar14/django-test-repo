fcb13f5cb1e1cde5feece11291b7c98104c4dbb1
qwazar14 <zenez89@gmail.com>
2022-03-27 18:52:46 +0300

    Removed mini-wiki

--- djangoTestProject/urls.py
+++ djangoTestProject/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('wiki/', include('micro_wiki.urls')),
     path('books/', include('books.urls')),
 ]
